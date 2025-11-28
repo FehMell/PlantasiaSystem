cc33463ba200d621bca2c9182bfa04bc13ddfb0b
liv-inn <liviaavelino2002@gmail.com>
2025-11-28 16:44:44 -0300

    backend scaffold

---
+++ backend/plantasiasystem/src/main/java/com/plantasia/plantasiasystem/PlantasiasystemApplication.java
@@ -0,0 +1,13 @@
+package com.plantasia.plantasiasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantasiasystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantasiasystemApplication.class, args);
+	}
+
+}
---
+++ backend/plantasiasystem/src/test/java/com/plantasia/plantasiasystem/PlantasiasystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantasia.plantasiasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantasiasystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
